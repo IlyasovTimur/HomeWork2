16a000b19e344698e9f8b03c563ad638409b51b0
varil <varilyasovtimur@gmail.com>
2022-08-27 22:04:48 +0700

    решаю задачи

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1.
+System.out.println("Задача 1");
+
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задача 2.
+        System.out.println("Задача 2");
+
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задача 3.
+        System.out.println("Задача 3");
+
+
+        dog = (int) (dog - 3.5);
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+
+    }
+}
