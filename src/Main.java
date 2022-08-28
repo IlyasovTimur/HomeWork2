26fb8c5b25fd6719682fb92289c61c9deec2b6c4
varil <varilyasovtimur@gmail.com>
2022-08-28 16:40:42 +0700

    решаю задачи

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,36 @@ public class Main {
         //Задача 1.
 System.out.println("Задача 1");
 
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
 
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println("значение переменной dog " + dog);
+        System.out.println("значение переменной cat " + cat);
+        System.out.println("значение переменной paper " + paper);
 
         //Задача 2.
         System.out.println("Задача 2");
-
 
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
 
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println("значение переменной dog " + dog);
+        System.out.println("значение переменной cat " + cat);
+        System.out.println("значение переменной paper " + paper);
 
         //Задача 3.
         System.out.println("Задача 3");
 
-
-        dog = (int) (dog - 3.5);
+        dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
 
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println("значение переменной dog " + dog);
+        System.out.println("значение переменной cat " + cat);
+        System.out.println("значение переменной paper " + paper);
 
-
+       //Задание 4.
     }
 }
