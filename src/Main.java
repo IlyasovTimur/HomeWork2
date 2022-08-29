e82b221ff4278ee9ec0f5ee1b2eac1be445a2eaa
varil <varilyasovtimur@gmail.com>
2022-08-29 11:55:39 +0700

    Домашняя работа №2 переменные (variable).

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,57 @@ System.out.println("Задача 1");
         System.out.println("значение переменной cat " + cat);
         System.out.println("значение переменной paper " + paper);
 
-       //Задание 4.
+       //Задача 4.
+            System.out.println("Задача 4");
+
+            var friend = 19;
+            System.out.println("значение переменной friend " + friend);
+            friend = friend + 2;
+            System.out.println("значение переменной friend " + friend);
+            friend = friend / 7;
+            System.out.println("значение переменной friend " + friend);
+
+            //Задача 5.
+            System.out.println("Задача 5");
+
+            var frog = 3.5;
+            System.out.println("значение переменной frog " + frog);
+            frog = frog * 10;
+            System.out.println("значение переменной frog " + frog);
+            frog = frog / 3.5;
+            System.out.println("значение переменной frog " + frog);
+            frog = frog + 4;
+            System.out.println("значение переменной frog " + frog);
+
+            //Задача 6.
+            System.out.println("Задача 6");
+
+            var boxerOne = 78.2;
+            var boxerTwo = 82.7;
+            var totalWeight = boxerOne + boxerTwo;
+            System.out.println("Общий вес двух бойцов = " + totalWeight + "кг");
+            var weightDifference = boxerTwo - boxerOne;
+            System.out.println("Разница в весе бойцов =" + weightDifference + "кг");
+
+            //задача 7.
+            System.out.println("Задача 7");
+            var subtraction = boxerTwo - boxerOne;
+            System.out.println("разница веса двух спортсменов вычисленная вычитанием = " + subtraction + "кг");
+            var remains = boxerTwo % boxerOne;
+            System.out.println("разница веса двух спортсменов вычисленная функцией остаток от деления = " + remains + "кг");
+
+            //задача 8.
+            System.out.println("Задача 8");
+            var totalHours = 640;
+            var totalWorkers = totalHours / 8;
+            System.out.println("Всего работников в компании – " + totalWorkers + " человек");
+            var moreWorkers = totalWorkers + 94; //Увеличили количество сотрудников.
+            var remainingTime = totalHours % moreWorkers; //Остаток от деления на цело, количество не задействованного времени.
+            var timeWorkers = totalHours - remainingTime; //Время, которое может быть поделено между всеми сотрудниками.
+            System.out.println("Если в компании работает " + moreWorkers + " человека, то всего " + timeWorkers + " часа работы может быть поделено между сотрудниками.");
+
+
+
+
     }
 }
